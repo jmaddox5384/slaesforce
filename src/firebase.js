581746e0083c5ba62f1fec61470b7d5136300ca8
Jay Maddox <jaymaddox5384@gmail.com>
2021-04-12 15:50:17 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwuzh0ckTna7UR00Gt5jBaw1QE8eqi60M",
+    authDomain: "salesforce-lead-5c7d3.firebaseapp.com",
+    projectId: "salesforce-lead-5c7d3",
+    storageBucket: "salesforce-lead-5c7d3.appspot.com",
+    messagingSenderId: "480121138749",
+    appId: "1:480121138749:web:c03f306c8c6ebc0edf0454"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+  export default db;
